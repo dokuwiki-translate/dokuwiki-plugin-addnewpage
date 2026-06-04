ac7ca95938d7f320c297c1bb87025408ae8c3d05
DokuWiki Translation Tool <admin@dokuwiki.org>
2026-06-04 14:52:48 +0200

    Translation update (sk) (#127)
    Co-authored-by: Wizzard <wizzardsk@gmail.com>

---
+++ lang/sk/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['namespaceRoot']         = 'Koreň';
+$lang['name']                  = 'Názov stránky';
+$lang['okbutton']              = 'Pridať stránku';
+$lang['nooption']              = 'Nemáte oprávnenie pridávať stránky';
---
+++ lang/sk/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['addpage_exclude']       = 'Vylúčené menné priestory (oddelené <code>;</code>)';
+$lang['addpage_showroot']      = 'Zobraziť koreňový menný priestor';
+$lang['addpage_hide']          = 'Pri použití predvoleného menného priestoru (syntax <code>{{NEWPAGE>ns}}</code>) skryť výber menného priestoru (ak nie je zaškrtnuté, výber bude obsahovať len podpriestory)';
+$lang['addpage_hideACL']       = 'Skryť formulár <code>{{NEWPAGE}}</code>, ak používateľ nemá oprávnenie pridávať stránky (ak nie je zaškrtnuté, zobrazí sa správa).';
+$lang['addpage_autopage']      = 'Nezobrazovať vstupné pole; prednastavený menný priestor sa považuje za úplné ID stránky (má zmysel pri zástupných symboloch dátumu).';
+$lang['addpage_createns']      = 'Povoliť vytváranie podpriestorov. Ak nie je zaškrtnuté, dvojbodky (<code>:</code>) v názve stránky sa nahradia podčiarkovníkom (<code>_</code>).';
