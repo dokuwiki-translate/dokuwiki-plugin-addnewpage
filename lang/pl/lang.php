23ad93a9f1cfe9b8227e68a33dc055af3bde87c8
Marek Adamski <fevbew@wp.pl>
2026-06-19 06:11:00 +0200

    Translation update (pl)

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -1,8 +1,13 @@
 <?php
-/*
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * polish language file
+ *
+ * @author Marek Adamski <fevbew@wp.pl>
  */
-$lang['namespaceRoot'] = "Główny";
-$lang['okbutton']      = "Dodaj stronę";
-$lang['nooption']      = "Nie masz uprawnień aby dodać nową stronę";
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['namespaceRoot']         = 'Główny';
+$lang['name']                  = 'Nazwa strony';
+$lang['okbutton']              = 'Dodaj stronę';
+$lang['nooption']              = 'Nie masz uprawnień aby dodać nową stronę';
--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -1,10 +1,16 @@
 <?php
-/*USE : UTF8*/
 
-/*
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * USE : UTF8
  * Polish language file
+ *
+ * @author Marek Adamski <fevbew@wp.pl>
  */
-$lang['addpage_exclude']  = "Wykluczone nazwy (oddzielone średnikiem)";
-$lang['addpage_showroot'] = "Pokaż nazwę głównego katalogu";
-$lang['addpage_hide']     = "Kiedy używasz składni {{NEWPAGE>[nazwa]}}: Ukryj nazwę grupy (niezaznaczone: pokaż tylko podnazwy)";
-$lang['addpage_hideACL']  = "Ukryj {{NEWPAGE}} jeżeli użytkownicy nie posiadają uprawnień dodawania nowych stron (pokaż komunikat jeśli niezaznaczone)";
+$lang['addpage_exclude']       = 'Wykluczone nazwy (oddzielone średnikiem)';
+$lang['addpage_showroot']      = 'Pokaż nazwę głównego katalogu';
+$lang['addpage_hide']          = 'Kiedy używasz składni {{NEWPAGE>[nazwa]}}: Ukryj nazwę grupy (niezaznaczone: pokaż tylko podnazwy)';
+$lang['addpage_hideACL']       = 'Ukryj {{NEWPAGE}} jeżeli użytkownicy nie posiadają uprawnień dodawania nowych stron (pokaż komunikat jeśli niezaznaczone)';
+$lang['addpage_autopage']      = 'Nie pokazuj pola wprowadzania danych, wstępnie skonfigurowana przestrzeń nazw jest traktowana jako pełny identyfikator strony (ma to sens w przypadku symboli zastępczych dat).';
+$lang['addpage_createns']      = 'Zezwól na tworzenie podprzestrzeni nazw. Jeśli odznaczone, dwukropki (<code>:</code>) w nazwie strony zostaną zastąpione podkreśleniem (<code>_</code>).';
