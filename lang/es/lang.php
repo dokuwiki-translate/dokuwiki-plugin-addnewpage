a703662a8a44df62aea66b08ddc942537d694c6b
Javi Valdivieso <tecnicoespaniol@gmail.com>
2026-06-30 19:04:09 +0200

    Translation update (es)

--- lang/es/lang.php
+++ lang/es/lang.php
@@ -1,8 +1,13 @@
 <?php
-/*
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * Spanish language file (Paraguay)
+ *
+ * @author Javi Valdivieso <tecnicoespaniol@gmail.com>
  */
-$lang['namespaceRoot'] = "Raiz";
-$lang['okbutton']      = "Agregar pagina";
-$lang['nooption']      = "Ud. no tiene privilegios para crear la pagina";
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['namespaceRoot']         = 'Raiz';
+$lang['name']                  = 'Nombre de página';
+$lang['okbutton']              = 'Agregar pagina';
+$lang['nooption']              = 'Ud. no tiene privilegios para crear la pagina';
--- lang/es/settings.php
+++ lang/es/settings.php
@@ -1,10 +1,16 @@
 <?php
-/*USE : UTF8*/
 
-/*
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * USE : UTF8
  * Spanish language file
+ *
+ * @author Javi Valdivieso <tecnicoespaniol@gmail.com>
  */
-$lang['addpage_exclude']  = "excluir directorio(namespace) separados por punto y coma (;)";
-$lang['addpage_showroot'] = "Mostrar directorio(namespace) raiz";
-$lang['addpage_hide']     = "cuando se usa la sintaxis {{NEWPAGE>[dir]}}: se oculta el selector de directorio (desmarcado: muestra solo sub-directorios (sub-namespaces))";
-$lang['addpage_hideACL']  = "si esta desmarcado, muesta un mensaje de error de privilegios. Si el usuario no tiene permiso de crear una pagina, simplemente se oculta el codigo {{NEWPAGE}}";
+$lang['addpage_exclude']       = 'excluir directorio(namespace) separados por punto y coma (;)';
+$lang['addpage_showroot']      = 'Mostrar directorio(namespace) raiz';
+$lang['addpage_hide']          = 'cuando se usa la sintaxis {{NEWPAGE>[dir]}}: se oculta el selector de directorio (desmarcado: muestra solo sub-directorios (sub-namespaces))';
+$lang['addpage_hideACL']       = 'si esta desmarcado, muesta un mensaje de error de privilegios. Si el usuario no tiene permiso de crear una pagina, simplemente se oculta el codigo {{NEWPAGE}}';
+$lang['addpage_autopage']      = 'No muestra el cuadro de entrada; el espacio de nombres preconfigurado se trata como un ID de página completa (tiene sentido con marcadores de fecha).';
+$lang['addpage_createns']      = 'Permite la creación de subespacios de nombres. Si no está marcada, los dos puntos (<code>:</code>) en el nombre de la página se reemplazarán por un guion bajo (<code>_</code>).';
